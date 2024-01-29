06c09ee330d58d7067caa51f870cc2b91ae6384e
Adam Drummond <adam.drummond9@gmail.com>
2024-01-29 23:33:38 +0000

    committee now gets data from public data folder

--- src/Pages/About/Committee/Committee.js
+++ src/Pages/About/Committee/Committee.js
@@ -1,17 +1,26 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./Committee.css";
-import React from "react";
-import data from './committee.json'
+import React, { useEffect, useState } from "react";
 
 export default function Committee(){
     document.title = "The Committee | BCS Isle of Man Section"
+    const [committee, setCommittee] = useState({committee:[{"name":"sample","role":"sample","qualifications":"sample","imgname":"sample"}]})
+
+    async function getCommittee() {
+        await fetch("https://raw.githubusercontent.com/AdamDIOM/BCSIOMSite/main/public/data/committee.json")
+        .then((response) => response.json())
+        .then((data) => setCommittee(data))
+    }
+    useEffect(() => {
+        getCommittee();
+    }, [])
 
     function CommitteeMember(props) {
         return (
             <Col xs={12} md={6} lg={4} key={props.key} className="text-center">
                 {
                 (props.member.imgname !== "")
-                ? <img src={"img/committee/" + props.member.imgname + ".png"} alt={"headshot of " + props.member.imgname} className="committee-headshot"/>
+                ? <img src={"/img/committee/" + props.member.imgname + ".png"} alt={"headshot of " + props.member.imgname} className="committee-headshot"/>
                 : <p></p>
                 }
                 
@@ -32,8 +41,9 @@ export default function Committee(){
                 </Row>
                 <Row className="committee-members">
                     {
-                        data.committee.map(member => {
-                            return <CommitteeMember key={member.name} member={member} />
+                        committee.committee.map(member => {
+                            console.log(member)
+                            return <CommitteeMember key={"key-" + member.name} member={member} />
                         })
                     }
                 </Row>
