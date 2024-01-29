925f8ddbad8359d37fbec9aae9cba80c35af9223
Adam Drummond <adam.drummond9@gmail.com>
2024-01-29 14:32:53 +0000

    added footer bolding

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,13 +5,13 @@ export default function Footer(){
     return(
             <Row className="footer">
                 <Col sm={4} xs={12} className="footerCol ">
-                    <a href="https://bcs.org" rel="noreferrer" target="_blank">BCS UK</a>
+                    <a href="https://bcs.org" rel="noreferrer" target="_blank"><b>BCS UK</b></a>
                 </Col>
                 <Col sm={4} xs={12} className="footerCol">
-                    <a href="https://www.bcs.org/membership-and-registrations/become-a-member/bcs-code-of-conduct/" rel="noreferrer" target="_blank">BCS Code of Conduct</a>
+                    <a href="https://www.bcs.org/membership-and-registrations/become-a-member/bcs-code-of-conduct/" rel="noreferrer" target="_blank"><b>BCS Code of Conduct</b></a>
                 </Col>
                 <Col sm={4} xs={12} className="footerCol">
-                    <a href="https://ti.to/bcs-iom/" rel="noreferrer" target="_blank">Events</a>
+                    <a href="https://ti.to/bcs-iom/" rel="noreferrer" target="_blank"><b>Events</b></a>
                 </Col>
             </Row>
     )
