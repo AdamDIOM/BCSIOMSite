c1281993cdbcaf5bbe0e49734d1eec3294dfc6eb
Adam Drummond <adam.drummond9@gmail.com>
2024-01-29 23:04:48 +0000

    added coming soon page for all unbuilt pages

---
+++ src/Pages/Error/ComingSoon/ComingSoon.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+import "./ComingSoon.css";
+
+export default function ComingSoon(){
+    return(
+        <Container className="content">
+            <h1>This page is coming soon.</h1>
+            <p>The content for this page has not yet been created. Please bear with.</p>
+        </Container>
+    )
+}
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,6 +3,7 @@ import Error404 from "../Pages/Error/404/404";
 import Home from "../Pages/Home/Home";
 import Events from "../Pages/WhatsOn/Events/Events";
 import Committee from "../Pages/About/Committee/Committee";
+import ComingSoon from "../Pages/Error/ComingSoon/ComingSoon";
 
 export default function Router(){
     return(
@@ -12,6 +13,10 @@ export default function Router(){
                 <Route path="/" element={<Home />} />
                 <Route path="/events" element={<Events />} />
                 <Route path="/committee" element={<Committee />} />
+                <Route path="/about-us" element={<ComingSoon />} />
+                <Route path="/papers" element={<ComingSoon />} />
+                <Route path="/get-online" element={<ComingSoon />} />
+                <Route path="/iom-tech" element={<ComingSoon />} />
             </Routes>
         </BrowserRouter>
     )
