7e4590d503d815b7ff2cb383dd78296ec2d9ca51
Adam Drummond <adam.drummond9@gmail.com>
2024-01-29 19:47:25 +0000

    converted dropdown nav to offcanvas

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,4 @@
-import { NavDropdown, Row } from "react-bootstrap";
+import { NavDropdown, Offcanvas, Row } from "react-bootstrap";
 import "./Header.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -8,13 +8,16 @@ export default function Header(){
     return(
         <Row className="header">
             <Navbar expand="md">
-                <Container>
+                <Container fluid>
                     <Navbar.Brand href="/">
                         <img className="header-logo" src="img/logo.png" alt="BCS Isle of Man Section logo" />
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="h-100">
-                        <Nav className="me-auto">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Offcanvas id="basic-navbar-nav" placement="end">
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title><b>BCS Isle of Man Section</b></Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Nav className="">
                             <Nav.Link href="/"><b>Home</b></Nav.Link>
                             <b><NavDropdown title="About Us" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="/about-us">
@@ -41,7 +44,13 @@ export default function Header(){
                             </NavDropdown></b>
                             <Nav.Link href="https://www.bcs.org/membership-and-registrations/become-a-member/"><b>Become a member</b></Nav.Link>
                         </Nav>
-                    </Navbar.Collapse>
+
+                        <Nav className="justify-content-end flex-grow-1 bottom-nav d-md-none">
+                            <Nav.Link href="https://bcs.org"><b>BCS UK</b></Nav.Link>
+                            <Nav.Link href="https://www.bcs.org/membership-and-registrations/become-a-member/bcs-code-of-conduct/"><b>BCS Code of Conduct</b></Nav.Link>
+                            <Navbar.Brand className="bottom-text">BCS (Isle of Man) Ltd is a registered company on the Isle of Man (number 111766C) and an Isle of Man registered charity (number 997)</Navbar.Brand>
+                        </Nav>
+                    </Navbar.Offcanvas>
                 </Container>
             </Navbar>
 
