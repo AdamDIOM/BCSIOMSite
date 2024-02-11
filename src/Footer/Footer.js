22ca8d22c49bf074171845004baad4014b0c3ef6
Adam Drummond <adam.drummond9@gmail.com>
2024-02-11 21:22:28 +0000

    added more legal info to footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,17 +4,21 @@ import "./Footer.css";
 export default function Footer(){
     return(
             <Row className="footer">
-                <Col sm={4} xs={12} className="footerCol ">
-                    <a href="https://bcs.org" rel="noreferrer" target="_blank"><b>BCS UK</b></a>
+                <Col md={3} sm={6} xs={12} className="footerCol ">
+                    <a href="https://bcs.org" rel="noreferrer" target="_blank"><b>BCS, The Chartered Institute for IT</b></a>
                 </Col>
-                <Col sm={4} xs={12} className="footerCol">
+                <Col md={3} sm={6} xs={12} className="footerCol">
                     <a href="https://www.bcs.org/membership-and-registrations/become-a-member/bcs-code-of-conduct/" rel="noreferrer" target="_blank"><b>BCS Code of Conduct</b></a>
                 </Col>
-                <Col sm={4} xs={12} className="footerCol">
-                    <a href="/events"><b>Events</b></a>
+                <Col md={3} sm={6} xs={12} className="footerCol">
+                    <a href="/events"><b>BCS IOM Events</b></a>
                 </Col>
-                <Col xs={12}>
-                    <p>BCS (Isle of Man) Ltd is a registered company on the Isle of Man (number 111766C) and an Isle of Man registered charity (number 997)</p>
+                <Col md={3} sm={6} xs={12} className="footerCol">
+                    <a href="https://www.bcs.org/legal-and-privacy-notices/data-privacy-notice/"><b>BCS UK Data Privacy Notice</b></a>
+                </Col>
+                <Col md={2} className="d-s-none"></Col>
+                <Col md={8} s={12}>
+                    <p>BCS (Isle of Man) Limited | Isle of Man Registered Company: number 111766C | Directors: K H Roosen, G Jones, O Cutajar, A C Drummond, S R R Wilson | Registered office: 4 Abbots Way, Abbotswood, Ballasalla, Isle of Man. IM9 3EQ | Isle of Man Registered Charity: number 997 | The name BCS, and any logos are property of BCS, The Chartered Institute for IT and are used with permission.</p>
                 </Col>
             </Row>
     )
