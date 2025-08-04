1329eaca55629ab34746dd8edf6bec3963cdaf33
AdamDIOM <github@adamd.im>
2025-08-04 21:01:03 +0100

    updated packages; changed committee page to link from google Sheets

--- src/Pages/About/Committee/Committee.js
+++ src/Pages/About/Committee/Committee.js
@@ -4,29 +4,32 @@ import React, { useEffect, useState } from "react";
 
 export default function Committee(){
     document.title = "The Committee | BCS Isle of Man Section"
-    const [committee, setCommittee] = useState({committee:[{"name":"sample","role":"sample","qualifications":"sample","imgname":"sample","linkedin":"sample"}]})
+    const [committee, setCommittee] = useState([{"name":"name","role":"sample","qualifications":"sample","imgid":"sample","linkedin":"sample"}])
+    const [loading, setLoading] = useState(true)
 
     async function getCommittee() {
-        await fetch("https://raw.githubusercontent.com/AdamDIOM/BCSIOMSite/main/public/data/committee.json")
+        await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLj0L5z9_BWxdJbW5jhtmSr75ANOYa5guo1MeXwNV5dzyTFCnO6a-gmlAPNhWz_5nzg65TfybFeqFtwWpZuULilxtCGk_tCL2DqVSFn7hIv-x0qYZFg4-FBPcXxIKLoqUA_I4dFKUUFq-pa_TE6E8_3eisafjGQY6jw41lwe4d1EiAnljU5PLQbS-6hgiexsXZhrS0PTEHfppSkF0mJ7gkxPFD0RF10rmN6VHVesHC0DhwHXmABeixaNwmZC9nIHLdQQ95mvIU6_2TSuoCssjtqIqpBSFA&lib=MQv3XoP1cugRDhwiCx8aDz8_gJC8hqBcP")
         .then((response) => response.json())
         .then((data) => setCommittee(data))
+        console.log(committee)
+        setLoading(false)
     }
     useEffect(() => {
         getCommittee();
     }, [])
 
-    function CommitteeMember(props) {
-        console.log(props.member)
+    function CommitteeMember({member, k}) {
+        console.log(member)
         return (
-            <Col xs={12} md={6} lg={4} key={props.key} className="text-center committee-member">
-                <img src={`/img/committee/${(props.member.imgname !== "") ? props.member.imgname : "NoPic"}.png`} alt={"headshot of " + props.member.imgname} className="committee-headshot"/>
-                {(props.member.role !== "")
-                ? <h2 className="role-title">{props.member.role}</h2>
-                : <p classname="role-title"><b>Committee member</b></p>
+            <Col xs={12} md={6} lg={4} key={k} className="text-center committee-member">
+                <img src={(member.imgid !== "") ? `https://lh3.googleusercontent.com/d/${member.imgid}` : "/img/committee/NoPic.png"} alt={"headshot of " + member.name} className="committee-headshot"/>
+                {(member.role !== "")
+                ? <h2 className="role-title">{member.role}</h2>
+                : <p className="role-title"><b>Committee member</b></p>
                 }
-                <p className="name-quals"><span><b>{props.member.name}</b> {props.member.qualifications} </span>
-                {(props.member.linkedin !== undefined && props.member.linkedin !== "") ?
-                    <a href={`https://linkedin.com/in/${props.member.linkedin}/`} target="_blank" rel="noreferrer">
+                <p className="name-quals"><span><b>{member.name}</b> {member.qualifications} </span>
+                {(member.linkedin !== undefined && member.linkedin !== "") ?
+                    <a href={`https://linkedin.com/in/${member.linkedin}/`} target="_blank" rel="noreferrer">
                         <img src="/img/linkedin-logo.png" alt="linkedin logo" className="linkedin-logo" />
                     </a>
                 : <React.Fragment />}
@@ -41,20 +44,35 @@ export default function Committee(){
                 <Row className="Intro">
                     <Col xs={12}>
                         <h1>BCS Isle of Man Committee</h1>
-                        <h2>**This page is due to be updated shortly following recent committee changes**</h2>
                         <p>Here are the members of the Isle of Man international section committee.</p>
                     </Col>
                 </Row>
-                <Row className="committee-members">
-                    {
-                        committee.committee.map(member => {
-                            return <CommitteeMember key={"key-" + member.name} member={member} />
-                        })
-                    }
-                </Row>
-                <Row>
-                    <p className="vacancy-disclaimer">To be considered for any above vacancies, you must be a BCS member and a resident of the Isle of Man; please get in touch with <a href="mailto:groups@bcs.uk">groups@bcs.uk</a> to submit your application for consideration, providing a cover letter and CV.</p>
-                </Row>
+                { loading ? (
+                    <>
+                        <div className="d-flex justify-content-center align-items-center w-100 h-100">
+                            <h2>Loading...</h2>
+                        </div>  
+                        <div className="d-flex justify-content-center align-items-center w-100 h-100">
+                            <div class="spinner-border" role="status"></div>
+                        </div>             
+                    </>      
+                ) : (
+                    <>
+
+                    
+                        <Row className="committee-members">
+                            {
+                                committee.map(member => {
+                                    console.log(member.name)
+                                    return <CommitteeMember k={member.name} member={member} />
+                                })
+                            }
+                        </Row>
+                        <Row>
+                            <p className="vacancy-disclaimer">To be considered for any above vacancies, you must be a BCS member and a resident of the Isle of Man; please get in touch with <a href="mailto:groups@bcs.uk">groups@bcs.uk</a> to submit your application for consideration, providing a cover letter and CV.</p>
+                        </Row>
+                </>
+                )}
             </Container>
         </React.Fragment>
     )
